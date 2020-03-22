ea678e112cf940ecdc909dd0a79f1d49dddb64fd
Q.Tran <tranlehaiquan@gmail.com>
2020-03-22 15:22:53 +0700

    fix script coverage

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,4 @@ module.exports = {
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
     'plugin:jest/recommended',
   ],
-  collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.{js,jsx}'],
 }
